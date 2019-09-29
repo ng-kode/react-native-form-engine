892f3397489b2e8d948f48a48c0cf3ffcad9e7e6
Richard-NG <richard.chihong.ng@gmail.com>
2019-09-29 11:07:34 +0800

    relocate templates

--- FormEngine.js
+++ FormEngine.js
@@ -1,35 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View, Text, TextInput, StyleSheet} from 'react-native';
 import produce from 'immer';
 import lodash from 'lodash';
 import validate from 'validate.js';
-
-const templates = {
-  TextField: ({label, onChange, hasError, errorText, touched, ...rest}) => {
-    const showError = touched && hasError;
-    const styles = StyleSheet.create({
-      wrapper: {marginBottom: 16},
-      label: {marginBottom: 4, fontWeight: 'bold'},
-      textInput: {
-        borderWidth: 1,
-        borderColor: 'lightgrey',
-        padding: 8,
-        borderRadius: 4,
-        ...(showError && {borderColor: 'red'}),
-      },
-      errorText: {color: 'red', fontSize: 12},
-    });
-
-    return (
-      <View style={styles.wrapper}>
-        <Text style={styles.label}>{label}</Text>
-        <TextInput style={styles.textInput} onChangeText={onChange} {...rest} />
-        {showError && <Text style={styles.errorText}>{errorText}</Text>}
-      </View>
-    );
-  },
-};
+import templates from './templates';
 
 const constraints = {
   email: {email: {message: '^INVALID_EMAIL'}},
